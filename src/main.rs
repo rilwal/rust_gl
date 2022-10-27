eef71fc9f41463d8c14a5fa814efabc247238965
Jason Light <rilwal2@gmail.com>
2022-10-28 00:14:25 +0900

    Work on Shaders and Programs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,56 @@
 pub mod renderer;
 
 use renderer::Renderer;
+use renderer::{Shader, ShaderType};
+
+const FRAG_SHADER_SRC : &str = r#"
+#version 330 core
+out vec4 FragColor;
+in vec3 something;
+uniform sampler2D tex;
+
+void main()
+{
+    vec2 windowPos = (gl_FragCoord.xy / 1024);
+	vec3 rgb = texture(tex, windowPos).rgb;
+    FragColor = vec4(rgb, 1.0f);
+} 
+
+"#;
+
+const VERT_SHADER_SRC : &str = r#"
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+"#;
+
 
 fn main() {
     let mut r = Renderer::initialize(1024, 768, "Hello");
+
+    let mut frag_shader = Shader::create(ShaderType::Fragment).unwrap();
+    frag_shader.compile(FRAG_SHADER_SRC).expect("Failed to compile frag shader");
+
+
+    let mut vert_shader = Shader::create(ShaderType::Vertex).unwrap();
+    vert_shader.compile(VERT_SHADER_SRC).expect("Failed to compile vert shader");
+
+
+    let mut p = renderer::Program::create().unwrap();
+    p.attach(&frag_shader).expect("Failed to attach shader to program");
+    p.attach(&vert_shader).expect("Failed to attach shader to program");
+
+    let res = p.link();
+
+    if let Err(error) = res {
+        println!("{}", error);
+    }
+
+
 
     while r.update() {
         // do something
--- src/renderer.rs
+++ src/renderer.rs
@@ -23,7 +23,7 @@ extern crate glm;
 extern crate glfw;
 
 use glfw::{Glfw, Window, WindowEvent, Key, Action, Context};
-use std::sync::mpsc::Receiver;
+use std::{sync::mpsc::Receiver, ffi::{c_void, CStr, CString}, str::from_utf8_unchecked, fmt};
 
 pub struct Renderer {
     glfw : Glfw,
@@ -31,6 +31,56 @@ pub struct Renderer {
     events : Receiver<(f64, WindowEvent)>,
 }
 
+#[derive(Debug, Clone)]
+pub enum GLError {
+    ResourceAllocationFailed,
+    ResourceNotAllocated,
+    ShaderCompileError(String),
+    ProgramLinkError(String)
+}
+
+
+impl fmt::Display for GLError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", match self {
+            Self::ResourceAllocationFailed  =>   "GlError: Resource allocation failed",
+            Self::ResourceNotAllocated      =>   "GlError: Resource not allocated",
+            Self::ShaderCompileError(_)     =>   "GlError: Shader compilation failed: ",
+            Self::ProgramLinkError(_)       =>   "GlError: Program linking failed: ",
+
+            _ => "Other GL Error"
+        },
+        match self {
+            Self::ShaderCompileError(s) => s,
+            Self::ProgramLinkError(s)   => s,
+            _ => ""
+        })
+    }
+}
+
+
+pub enum ShaderType {
+    Fragment,
+    Vertex,
+    Geometry, 
+}
+
+
+pub enum GLType {
+    Vector3,
+    Vector4,
+    Float
+}
+
+pub struct Shader {
+    id: u32,
+    kind: ShaderType
+}
+
+
+pub struct Program {
+    id: u32,
+}
 
 
 impl Renderer {
@@ -68,3 +118,112 @@ impl Renderer {
         !self.window.should_close()
     }
 }
+
+
+impl Shader {
+    pub fn create(kind : ShaderType) -> Result<Self, GLError> {
+        let mut shader = 0;
+        unsafe {
+            shader = gl::CreateShader(match kind {
+                ShaderType::Fragment => gl::FRAGMENT_SHADER,
+                ShaderType::Vertex => gl::VERTEX_SHADER,
+                ShaderType::Geometry => gl::GEOMETRY_SHADER
+            });
+        }
+
+        match shader {
+            0 => Err(GLError::ResourceAllocationFailed),
+            _ => Ok(Self {id: shader, kind: kind})
+        }
+    }
+
+    pub fn compile(&mut self, source : &str) -> Result<(), GLError> {
+        if self.id == 0 {
+            return Err(GLError::ResourceNotAllocated)
+        }
+
+        unsafe {
+            let src_ptr = source.as_ptr() as *const i8;
+            let src_len = source.len() as i32;
+
+            gl::ShaderSource(self.id, 1, &src_ptr, &src_len);
+            gl::CompileShader(self.id);
+
+            let mut compile_status = 0i32;
+            gl::GetShaderiv(self.id, gl::COMPILE_STATUS, &mut compile_status as *mut i32);
+
+            if compile_status != gl::TRUE as i32 {
+                // If we fail to compile, return a GLError::ShaderCompileError with the erorr log
+                let mut info_log_len = 0i32;
+                gl::GetShaderiv(self.id, gl::INFO_LOG_LENGTH, &mut info_log_len as *mut i32);
+
+                let mut info_log_bytes = vec![0u8; info_log_len as usize];
+                gl::GetShaderInfoLog(self.id, info_log_len, std::ptr::null_mut(), info_log_bytes.as_mut_ptr() as *mut i8);
+                
+                let info_log_str = String::from_utf8_unchecked(info_log_bytes);
+
+                return Err(GLError::ShaderCompileError(info_log_str))
+            }
+        }
+
+        Ok(())
+    }
+}
+
+
+impl Program {
+    pub fn create() -> Result<Self, GLError> {
+        let mut program = 0;
+
+        unsafe {
+            program = gl::CreateProgram();
+        }
+
+        match program {
+            0 => Err(GLError::ResourceAllocationFailed),
+            _ => Ok(Self{id: program})
+        }
+    }
+
+    pub fn attach(&mut self, shader : &Shader) -> Result<(), GLError>{
+        unsafe {
+            gl::AttachShader(self.id, shader.id);
+
+            if gl::GetError() != gl::NO_ERROR {
+                return Err(GLError::ResourceAllocationFailed); // TODO: Better error here!!!
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn link(&mut self) -> Result<(), GLError> {
+        if self.id == 0 {
+            return Err(GLError::ResourceNotAllocated)
+        }
+
+        unsafe {
+            
+            gl::LinkProgram(self.id);
+
+            let mut link_status = 0i32;
+            gl::GetProgramiv(self.id, gl::LINK_STATUS, &mut link_status as *mut i32);
+
+            if link_status != gl::TRUE as i32 {
+                // If we fail to compile, return a GLError::ShaderCompileError with the erorr log
+                let mut info_log_len = 0i32;
+                gl::GetProgramiv(self.id, gl::INFO_LOG_LENGTH, &mut info_log_len as *mut i32);
+
+                let mut info_log_bytes = vec![0u8; info_log_len as usize];
+                gl::GetProgramInfoLog(self.id, info_log_len, std::ptr::null_mut(), info_log_bytes.as_mut_ptr() as *mut i8);
+                
+                let info_log_str = String::from_utf8_unchecked(info_log_bytes);
+
+                return Err(GLError::ProgramLinkError(info_log_str))
+            }
+        }
+
+        Ok(())
+    }
+}
+
